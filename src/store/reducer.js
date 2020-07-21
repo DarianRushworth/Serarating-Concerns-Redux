b44719d21d9c6a2c532be83ce7902f0ad96d82d0
DarianRushworth <darianerrolrushworth@gmail.com>
2020-07-21 13:42:48 +0200

    Set-up and intialization

---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    // TODO
+    hello: "world"
+  };
+  
+  export default function reducer(state = initialState, action) {
+    switch (action.type) {
+      default: {
+        return state;
+      }
+    }
+  }
